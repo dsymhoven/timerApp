500a0f2afcaf604fbfad391f6d698fa6915a69cf
David Symhoven <symhoven@googlemail.com>
2017-07-29 18:23:45 +0200

    shows and hides picker VIew in IntervalViewController

--- TimerApp/IntervalViewController.swift
+++ TimerApp/IntervalViewController.swift
@@ -10,11 +10,109 @@ import UIKit
 
 class IntervalViewController: UIViewController {
 
+    // MARK: Properties
+    fileprivate let pickerData = ["1", "2", "3", "4", "5"]
+    fileprivate var numberOfRounds: Int?
+    fileprivate var pickerViewIsVisible = false
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var roundsButton: UIButton!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var pickerViewContainer: UIView!
+    @IBOutlet weak var IntervalButton: UIButton!
+    @IBOutlet weak var PauseButton: UIButton!
+
+    // MARK: IBActions
+    @IBAction func roundsButtonPressed(_ sender: UIButton) {
+        hideAndShowPickerView()
+    }
+    
+    @IBAction func IntervalButtonPressed(_ sender: UIButton) {
+        hideAndShowPickerView()
+    }
+    
+    @IBAction func pauseButtonPressed(_ sender: UIButton) {
+        hideAndShowPickerView()
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    }
+    
+    // MARK: User functions
+    
+    fileprivate func makePickerViewDisappear() {
+        pickerViewContainer.alpha = 0.0
+        pickerViewContainer.frame.origin.y = UIScreen.main.bounds.height
+        pickerViewIsVisible = false
+    }
+    
+    fileprivate func makePickerViewAppear() {
+        pickerViewContainer.alpha = 1.0
+        guard let tabHeight = tabBarController?.tabBar.frame.size.height else{return}
+        pickerViewContainer.frame.origin.y = UIScreen.main.bounds.height - 216 - tabHeight
+        pickerViewIsVisible = true
+    }
+    
+    fileprivate func hideAndShowPickerView() {
+        UIView.animate(withDuration: pickerViewIsVisible ? 0.5 : 0, animations: {[weak weakSelf = self] in
+            weakSelf?.makePickerViewDisappear()
+        }) {(completed) in
+            if completed {
+                UIView.animate(withDuration: 0.5, animations: {[weak weakSelf = self] in
+                    weakSelf?.makePickerViewAppear()
+                })
+            }
+        }
     }
 
 
+}
+
+
+extension IntervalViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+}
+
+extension IntervalViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        guard let rounds = Int(pickerData[row]) else {return}
+        numberOfRounds = rounds
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let pickerLabel = UILabel()
+        let titleData = pickerData[row] + " "
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont.systemFont(ofSize: 24), NSForegroundColorAttributeName:UIColor.white])
+        pickerLabel.attributedText = myTitle
+        pickerLabel.textAlignment = .center
+        
+        // hide picker view selection lines
+        // pickerView.showsSelectionIndicator is pre iOS 7 only
+        pickerView.subviews[1].isHidden = true
+        pickerView.subviews[2].isHidden = true
+        
+        return pickerLabel
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PickerViewConstants.rowHeight
+    }
 }
