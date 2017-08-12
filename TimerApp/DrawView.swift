57db5df9bdcb906609cbc5d18dfd1ebb9ff6139e
David Symhoven <symhoven@googlemail.com>
2017-08-12 11:32:10 +0200

    fill background of drawView

--- TimerApp/DrawView.swift
+++ TimerApp/DrawView.swift
@@ -13,18 +13,29 @@ class DrawView: UIView {
     override func draw(_ rect: CGRect)
     {
         let context = UIGraphicsGetCurrentContext()
+        let lineWidth = CGFloat(4.0)
         let upperLeftPoint = CGPoint(x: self.bounds.origin.x, y: self.bounds.origin.y + 20)
         let upperRightPoint = CGPoint(x: self.bounds.width, y: self.bounds.origin.y + 20)
         let upperControlPoint = CGPoint(x: self.bounds.width / 2, y: self.bounds.origin.y)
         
-        let lowerLeftPoint = CGPoint(x: self.bounds.origin.x, y: self.bounds.height)
-        let lowerRightPoint = CGPoint(x: self.bounds.width, y: self.bounds.height)
+        let lowerLeftPoint = CGPoint(x: self.bounds.origin.x, y: self.bounds.height - lineWidth)
+        let lowerRightPoint = CGPoint(x: self.bounds.width, y: self.bounds.height - lineWidth)
         let lowerControlPoint = CGPoint(x: self.bounds.width / 2, y: self.bounds.height - 20)
         
         
-        
-        context?.setLineWidth(4.0)
+
+        let myBezier = UIBezierPath()
+        myBezier.move(to: upperLeftPoint)
+        myBezier.addQuadCurve(to: upperRightPoint, controlPoint: upperControlPoint)
+        myBezier.addLine(to: lowerRightPoint)
+        myBezier.addQuadCurve(to: lowerLeftPoint, controlPoint: lowerControlPoint)
+        myBezier.close()
+        context?.setLineWidth(lineWidth)
+        UIColor(red:0.11, green:0.22, blue:0.19, alpha:1.0).setFill()
+        myBezier.fill()
+
         context?.setStrokeColor(UIColor(red:0.02, green:0.84, blue:0.63, alpha:1.0).cgColor)
+        context?.strokePath()
         context?.move(to: upperLeftPoint)
         context?.addQuadCurve(to: upperRightPoint,
                               control: upperControlPoint)
@@ -32,6 +43,7 @@ class DrawView: UIView {
         context?.addQuadCurve(to: lowerRightPoint,
                               control: lowerControlPoint)
         context?.strokePath()
+        
     }
 
 
