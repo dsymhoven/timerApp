df296aa1b246e186edac8261ed56b67347847594
David Symhoven <symhoven@googlemail.com>
2017-08-12 17:06:24 +0200

    finish custom progress bar

--- TimerApp/ProgressViewContainer.swift
+++ TimerApp/ProgressViewContainer.swift
@@ -11,20 +11,27 @@ import UIKit
 class ProgressViewContainer: UIView {
     
     var numberOfRounds = 4
-    var lengthOfPause = 30
+    var lengthOfPause = 15
+    var lengthOfInterval = 30
 
     override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
         let lineHeight = CGFloat(8)
+        let totalTime = numberOfRounds * (lengthOfPause + lengthOfInterval)
+        let pauseWidth = (self.bounds.width / CGFloat(totalTime)) * CGFloat(lengthOfPause)
         
         context?.setLineWidth(1)
         context?.setStrokeColor(UIColor.Background.componentsBackground.cgColor)
         
         for rounds in 1..<numberOfRounds {
-            let pointLow = CGPoint(x: CGFloat(rounds) * (self.bounds.width / CGFloat(numberOfRounds)), y: self.bounds.height / 2 + lineHeight / 2)
-            let pointTop = CGPoint(x: CGFloat(rounds) * (self.bounds.width / CGFloat(numberOfRounds)), y: self.bounds.height / 2 - lineHeight / 2)
-            context?.move(to: pointLow)
-            context?.addLine(to: pointTop)
+            let intervalPointLow = CGPoint(x: CGFloat(rounds) * (self.bounds.width / CGFloat(numberOfRounds)), y: self.bounds.height / 2 + lineHeight / 2)
+            let intervalPointTop = CGPoint(x: CGFloat(rounds) * (self.bounds.width / CGFloat(numberOfRounds)), y: self.bounds.height / 2 - lineHeight / 2)
+            let pausePointLow = CGPoint(x: intervalPointLow.x + pauseWidth, y: self.bounds.height / 2 + 1)
+            let pausePointTop = CGPoint(x: intervalPointTop.x + pauseWidth, y: self.bounds.height / 2 - 1)
+            context?.move(to: intervalPointLow)
+            context?.addLine(to: intervalPointTop)
+            context?.move(to: pausePointLow)
+            context?.addLine(to: pausePointTop)
             context?.strokePath()
         }
     }
